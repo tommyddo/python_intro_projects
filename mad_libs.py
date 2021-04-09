284c3d21a1e077be9f7f49a1edb34e4cefb192b1
Tommy <tommy.do196@outlook.com>
2021-04-09 14:47:22 -0500

    Added more details. Needs to adjust in future to account for part of speech as well as if they enter int or other parts of speech instead of correct one

--- mad_libs.py
+++ mad_libs.py
@@ -1,2 +1,18 @@
-print('hello world')
-print(1+1)
+# Mad Libs Game 1
+print("Fill in the ____ with your chosen word "
+      "based on the part of speech given.")
+print()
+print('Please excuse ____ who is far too ____ to attend ____ class.')
+print()
+name=input('Please excuse ____ ' 'Enter a name ')
+print("Please excuse {} who is far too ____ to attend ____ class."
+      .format(name))
+print()
+adjective=input('who is far too ---- ' 'Enter an adjective ')
+print('Please excuse {} who is far too {} to attend ____ class.'
+      .format(name,adjective))
+print()
+noun=input('to attend ____ class. ' 'Enter a noun ')
+print("Please excuse {} who is far too {} to attend {} class."
+      .format(name,adjective,noun))
+
